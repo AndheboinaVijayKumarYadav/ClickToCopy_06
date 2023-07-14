0b61be824c7f8ce6a5479885a7f16e839a0ef33c
AndheboinaVijayKumarYadav <andheboinavijaykumaryadav@gmail.com>
2023-07-14 07:07:53 +0530

    Create script.js with logic in it

---
+++ script.js
@@ -0,0 +1,31 @@
+// variables
+const copyBtn = document.querySelector(".copy-btn"),
+      coupon = document.querySelector(".coupon-text");
+
+
+// adding the event listener
+copyBtn.addEventListener('click',copyText);
+
+
+// function 
+function copyText(e) {
+    e.preventDefault();
+
+   /*  
+    // here select the input field
+    coupon.select();
+    // setting the range to be considered to copy
+    coupon.setSelectionRange(0,999999);
+
+    // Copying text to clipboard using execCommand() method
+    document.execCommand('copy');
+     */
+
+    navigator.clipboard.writeText(coupon.value).then(() => {
+        copyBtn.innerText = 'Copied!!!';
+
+        // setting time for changing the button text back to copy
+        setTimeout(() => { copyBtn.innerText = 'Copy'},300)
+    });
+    
+}
